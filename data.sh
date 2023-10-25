df47cdbb4e3c071c7f3b425932857d6bbe1d1ad0
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-10-25 21:02:03 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,4 +2,4 @@ git clone https://github.com/GOUSERABBANI44/flask-library-app.git
 cd flask-library-app
 sudo apt install -y python3-pip
 pip3 install -r requirements.txt
-python3 app.py
+nohup python3 app.py &
