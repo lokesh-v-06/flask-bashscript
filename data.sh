d036bd35f188d4af601292a4c961215820fade63
lokesh-v-06 <78427101+lokesh-v-06@users.noreply.github.com>
2023-10-30 08:22:51 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/GOUSERABBANI44/flask-library-app.git
+git clone https://github.com/lokesh-v-06/flask-library-app.git
 cd flask-library-app
 sudo apt install -y python3-pip
 pip3 install -r requirements.txt
